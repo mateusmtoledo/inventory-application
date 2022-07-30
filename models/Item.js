e6a97bbde3f158a147d4f2aa95ceb4c8d2da6644
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-07-29 22:09:46 -0300

    Update models

--- models/Item.js
+++ models/Item.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
+  _id: { type: Number, required: true },
   name: { type: String, required: true, maxLength: 100 },
   description: { type: String },
+  category: { type: Number, ref: 'Category', required: true },
   price: { type: Number, required: true },
   numberInStock: { type: Number, required: true },
+  image: { type: String, required: true },
 });
 
 ItemSchema.virtual('url').get(() => {
