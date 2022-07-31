d274d9e07b6e8ae255486264692c9301ee0fc9cf
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-07-30 21:48:38 -0300

    Add category details pages

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,7 +1,44 @@
 const Category = require('../models/Category');
+const Item = require('../models/Item');
+const async = require('async');
 
 exports.categoryList = (req, res, next) => {
   Category.find({}).limit(10).exec((err, categories) => {
+    if(err) {
+      next(err);
+      return;
+    }
     res.render('categoryList', { title: 'Categories', categories })
   });
-}
+};
+
+exports.categoryDetails = (req, res, next) => {
+  const { categoryId } = req.params;
+  async.parallel({
+    category(callback) {
+      Category.findById(categoryId, (err, category) => {
+        if(err) {
+          callback(err, null);
+          return;
+        }
+        callback(null, category);
+      })
+    },
+    items(callback) {
+      Item.find({ category: categoryId }, 'name image').limit(10).exec((err, items) => {
+        if(err) {
+          callback(err,null);
+          return;
+        }
+        callback(null, items);
+      });
+    },
+  }, (err, results) => {
+    if(err) {
+      next(err);
+      return;
+    }
+    const { category, items } = results
+    res.render('categoryDetails', { category, items });
+  });
+};
